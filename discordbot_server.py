52248cfa1dd2a0a8a4bc4543ed8838bd29f9d798
Mikuto <mikuto1227k@gmail.com>
2024-02-05 17:38:59 +0900

    それっぽく出力できるようになったのでコミットします。 ログ出力はこれでいったん終わりです。

--- discordbot_server.py
+++ discordbot_server.py
@@ -1,17 +1,12 @@
-<<<<<<< HEAD
-#外部モジュールのimport
-import discord
-#import logging
-=======
 # 外部モジュールのimport
 import discord      # discordBotで一番必要
+from logging import INFO, Logger
+import logging
 from dotenv import load_dotenv # .envファイルを読み取るために導入
 import os           # OS操作用
->>>>>>> master
 
 # 自作モジュールのimport
 import constant     # 定数を定義できるクラスがある
-
 
 # .envファイルの呼び出し
 load_dotenv()
@@ -21,6 +16,15 @@ class const(constant.Constant):
 
     #botのトークン
     TOKEN: str = os.getenv('TOKEN')
+
+# rootロガーとハンドラのlevelを下げる(logging)
+logger:Logger = logging.getLogger("myLogger")
+logger.setLevel(INFO)
+dt_fmt = '%Y-%m-%d %H:%M:%S'
+formatter = logging.Formatter('{asctime} {levelname:<8} {funcName:<16} {message}', dt_fmt, style='{')
+logging.lastResort.setFormatter(formatter)
+logging.lastResort.setLevel(INFO)
+
 
 #インテント定義
 #このボットではすべてを有効にする
@@ -33,8 +37,7 @@ client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     #ターミナルへ起動報告
-    #logging.info("起動しました")
-    pass
+    logger.info("起動しました")
 
 #メッセージ受信時の動作
 @client.event
